627209bf0255a37aba623a293dca88ab0512f938
simpli11ity <51165497+simpli11ity@users.noreply.github.com>
2019-11-20 21:50:41 +0000

    Update starter.py

--- scripts/starter.py
+++ scripts/starter.py
@@ -4,11 +4,12 @@ import cv2
 from matplotlib import pyplot as plt
 from matplotlib import image as image
 import easygui
+import imutils
 
 #task histogram
 def  getImage():
-		f = easygui.fileopenbox()
-		I = cv2.imread(f)
+		#f = easygui.fileopenbox()
+		I = cv2.imread("amoeba.jpg")
 		return I
 
 # TODO Change this to include better filter
@@ -16,6 +17,8 @@ def getChannelet(I):
 		# convert to YUV
 		YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
 		Single = YUV[:,:,2]
+		# G works on other images
+		#G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 		# Pass through a high pass filter
 		k =np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]], dtype=float)
 		Filter =cv2.filter2D(Single,ddepth=-1,kernel=k)
@@ -35,22 +38,14 @@ def getBoundary(B):
 		shape = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
 		# Create the boundary
 		Boundary = cv2.morphologyEx(B,cv2.MORPH_GRADIENT,shape)
-		# Get the regions of interest (Cells)
 		return Boundary
 
-
-# Create the contours and just bound a rectangle on a clean image of the first
-def getcontours(Boundary,ROI,Rect):
-		# Find the contours
-		c, hierarchy = cv2.findContours(Boundary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-		# Sort contours
-		c = sorted(c, key=cv2.contourArea, reverse=True)
-		# Draw contours
-		Z = cv2.drawContours(ROI, c, -1, (0,255,0), 2)
+def bindRectangle(I,C):
+		c = sorted(C, key=cv2.contourArea, reverse=True)
 		x,y,w,h=cv2.boundingRect(c[0])
-		cv2.rectangle(Rect,(x,y),(x+w,y+h),(0,255,255),2)
-		return Rect
-
+		cv2.rectangle(I,(x,y),(x+w,y+h),(0,255,255),2)
+		return I
+  
 def findXY(Boundary,I):
 	c, hierarchy = cv2.findContours(Boundary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 	c = sorted(c, key=cv2.contourArea, reverse=True)
@@ -65,6 +60,11 @@ def crop(Boundary,I):
 	croppedimg = I[y:y + h, x:x + w]
 	return croppedimg
 
+# Create the contours and just bound a rectangle on a clean image of the first
+def getcontours(Boundary):
+	c, hierarchy = cv2.findContours(Boundary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	return c
+
 def getThresh(I):
 	YUV = cv2.cvtColor(I, cv2.COLOR_BGR2YUV)
 	V = YUV[:, :, 2]
@@ -78,6 +78,20 @@ def findNucleus(thresh):
 	contours, hierarchy = cv2.findContours(NewMaskout, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	count = contours[0]
 	return count
+
+def findCellMembrance(C,ROI,Current):
+	C = sorted(C, key=cv2.contourArea, reverse=True)
+	# Calculate the center of the first contour aka largest
+	M = cv2.moments(C[0])
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+	cv2.putText(Current, "Membrane", (cX + int(cX/4), cY),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	# places next to membrane
+	cv2.putText(Current, "Cytoplasm", (cX + int(cX/6), cY+40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 212),2)
+	# Draw contours
+	I = cv2.drawContours(Current, C[0], -1, (0,0,255), 2)
+
+	return I
 
 def DrawNucleus(count,arrayXYWH):
 	disx = arrayXYWH[0]
@@ -101,12 +115,16 @@ Z = I
 B = getChannelet(I)
 Boundary = getBoundary(B)
 ROI = getROI(B,I)
-Contours = getcontours(Boundary,ROI,Z)
+C = getcontours(Boundary)
+I = bindRectangle(Z,C)
 arrayXYWH = findXY(Boundary,I)
 Croppedimg = crop(Boundary,I)
+
 threshold = getThresh(Croppedimg)
 getCont = findNucleus(threshold)
 drawNucleus = DrawNucleus(getCont, arrayXYWH)
-show(drawNucleus)
 
+
+m = findCellMembrance(C,ROI,drawNucleus)
+show(m)
 key = cv2.waitKey(0)
